4433afcf5f3dd4a69caabcaf4ff2bb33bf6f66f9
Olexandr Z <zeeelensky@gmail.com>
2022-01-22 23:42:11 +0200

    change fix and repeat

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 import express from 'express';
 import path from 'path';
 import { config } from 'dotenv';
-
 import ffmpeg from 'fluent-ffmpeg';
 import pathToFfmpeg from 'ffmpeg-static';
 import ffprobe from 'ffprobe-static';
-
-import { randomUUID } from 'crypto';
+import multer from 'multer';
 
 config();
 
@@ -15,46 +13,68 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const __dirname = path.resolve();
 
-
-app.get('/audio', ( req, res ) => {
-  res.status(200).send({
-    message: 'Hello from audio',
-  });
+const storageConfig = multer.diskStorage({
+  destination: ( req, file, cb ) => {
+    cb(null, 'uploads');
+  },
+  filename: ( req, file, cb ) => {
+    cb(null, file.originalname);
+  },
 });
+app.use(express.static(__dirname));
+app.use(multer({ storage: storageConfig }).single('filedata'));
+
+
 
 app.post('/audio', ( req, res ) => {
-  const fname = `${os.tmpdir()}/${randomUUID()}.mp3`
-
-  if (req.start < req.stop) {
-    res.sendStatus(400).send("start must be less than stop")
+  if ( !req.file ) {
+    return res.status(400).send({
+      message: 'An error has occurred while uploading the file',
+    }).end();
   }
 
-  ffmpeg(fname)
+  let { start, stop } = req.query;
+  start = Number(start);
+  stop = Number(stop);
+
+  const filePath = req.file.path;
+  if ( start >= stop ) {
+    res.status(400).send('start must be less than stop');
+  }
+
+  ffmpeg(filePath)
     .setFfmpegPath(pathToFfmpeg)
     .setFfprobePath(ffprobe.path)
-    .output(fname)
-    .setStartTime(req.start)
-    .setDuration(req.stop - req.start)
+    .output(filePath)
+    .setStartTime(start * 1)
+    .setDuration(stop - start)
     .withAudioCodec('copy')
-    .on('end', function (err) {
-      if (!err) {
-          console.log("conversion Done");
-          res.sendStatus(200).send("nice");
-          resolve();
+    .on('end', function( err ) {
+      if ( !err ) {
+        console.log('conversion Done');
+        res.status(200).sendFile(path.join(__dirname, filePath));
       }
     })
-    .on('error', function (err) {
-      res.sendStatus(400).send(err);
-      reject(err);
+    .on('error', function( err ) {
+      res.status(400).send({
+        message: 'An error has occurred while changing the file',
+      }).end();
     })
     .run();
-
-  res.status(200).send({
-    
-  });
 });
 
-app.use(express.static(path.join(__dirname, 'src')));
+
+app.get('/style.css', ( req, res ) => {
+  res.sendFile(path.join(__dirname, 'src', 'style.css'));
+});
+
+app.get('/index.js', ( req, res ) => {
+  res.sendFile(path.join(__dirname, 'src', 'index.js'));
+});
+
+app.get('/', ( req, res ) => {
+  res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`The server is running on port:${ PORT }...`);

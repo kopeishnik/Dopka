20db94ae9559f9d5f8bdadad4ceae651a1ba467f
Olexandr Z <zeeelensky@gmail.com>
2022-01-22 20:59:35 +0200

    rework

--- server.js
+++ server.js
@@ -6,49 +6,81 @@ import http from 'http';
 import ffmpeg from 'fluent-ffmpeg';
 import pathToFfmpeg from 'ffmpeg-static';
 import ffprobe from 'ffprobe-static';
+import { config } from 'dotenv';
+import fs from 'fs/promises';
+import path from 'path';
 
-const server = http.createServer((req, res) => {
-    if (req.method === 'POST') {
-        var fname = `tmp/${req.files.mp3.name}`
-        req.files.mp3.mv(fname, function(error) {
-            if (error) return res.sendStatus(500).send(err)
+const __dirname = path.resolve();
+
+config();
+
+const handleAudio = ( req, res ) => {
+  console.log(req);
+  if ( req.method === 'POST' ) {
+    const fname = `tmp/${ req.files.mp3.name }`;
+    req.files.mp3.mv(fname, function( error ) {
+      if ( error ) return res.sendStatus(500).send(err);
+    });
+
+    if ( req.start < req.stop ) {
+      res.sendStatus(400).send('start must be less than stop');
+    } else {
+      ffmpeg(fname)
+        .setFfmpegPath(pathToFfmpeg)
+        .setFfprobePath(ffprobe.path)
+        .output(fname)
+        .setStartTime(req.start)
+        .setDuration(req.stop - req.start)
+        .withAudioCodec('copy')
+        .on('end', function( err ) {
+          if ( !err ) {
+            console.log('conversion Done');
+            resolve();
+          }
         })
-
-        if (req.start < req.stop) {
-            res.sendStatus(400).send("start must be less than stop")
-        }
-        else {
-            ffmpeg(fname)
-                .setFfmpegPath(pathToFfmpeg)
-                .setFfprobePath(ffprobe.path)
-                .output(fname)
-                .setStartTime(req.start)
-                .setDuration(req.stop - req.start)
-                .withAudioCodec('copy')
-                .on('end', function (err) {
-                    if (!err) {
-                        console.log('conversion Done');
-                        resolve();
-                    }
-                })
-                .on('error', function (err) {
-                    console.log('error: ', err);
-                    reject(err);
-                })
-                .run();
-        }
-        res.attachment(fname)
-        res.setHeader('Content-type', 'text/plain');
-        res.contentType('audio/mp3')
+        .on('error', function( err ) {
+          console.log('error: ', err);
+          reject(err);
+        })
+        .run();
     }
-    else {
-        res.writeHead(500, { 'Content-Type': 'application/json' })
-        res.end(JSON.stringify({ message: 'Cannot do this' }))
-    }
-})
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+    res.attachment(fname);
+    res.setHeader('Content-type', 'text/plain');
+    res.contentType('audio/mp3');
+  } else {
+    res.writeHead(500, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Cannot do this' }));
+  }
+};
 
-const PORT =  process.env.PORT || 5000
+const handleStatic = ( req, res ) => {
+  fs.readFile(path.join(__dirname, 'src', 'index.html'))
+    .then(contents => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+      res.setHeader('Content-Type', 'text/html');
+      res.writeHead(200);
+      res.end(contents);
+    });
+};
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
-module.exports = server;
+const server = http.createServer(( req, res ) => {
+  const url = req.url;
+  if ( url === '/audio' ) {
+    handleAudio(req, res);
+  } else {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'origin, content-type, accept');
+    res.setHeader('Content-Type', 'text/plain');
+    res.writeHead(200);
+    res.end('Hello from server');
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${ PORT }`));
+
